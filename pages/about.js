2a4d1de987c624d6eaa74c96063c96d335ac64e6
MZ <mzaremba@gmx.net>
2022-01-12 18:14:49 +0200

    Edited some text

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,15 @@
-import Link from 'next/link'
+import Link from "next/link";
 export default function About() {
   return (
     <div>
       <div>About us</div>
+      <div>It works, doesn't it?</div>
       <div>
-        Back to{' '}
-        <Link href="/" as={process.env.BACKEND_URL + '/'}>
+        Back to{" "}
+        <Link href="/" as={process.env.BACKEND_URL + "/"}>
           <a>Home</a>
         </Link>
       </div>
     </div>
-  )
+  );
 }
